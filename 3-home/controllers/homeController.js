cbc90847f629c426947b14a1dc6ee40db76300f4
Santiago Cotto <scotto@goshops.ai>
2023-07-25 00:00:02 -0300

    r

--- 3-home/controllers/homeController.js
+++ 3-home/controllers/homeController.js
@@ -2,10 +2,6 @@
 
 async function homeController(){
 
-    console.log('Home Controller');
-    
-    // await window.gsSDK.getContent('home-banner');
-    // await window.gsSDK.getContent('home-reco');
-
     await window.gsSDK.getContentByContext('home');
 }
+
