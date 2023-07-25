6e509bb314369a91c2ee0be20dfdfcd1bd8157d2
Santiago Cotto <scotto@goshops.ai>
2023-07-25 17:01:57 -0300

    fixes

--- 3-home/controllers/homeController.js
+++ 3-home/controllers/homeController.js
@@ -1,4 +1,15 @@
 
+async function handleHeartClick(event, productId) {
+    event.preventDefault();
+    
+    const heartIcon = event.currentTarget.querySelector("i.bi-heart");
+    heartIcon.classList.toggle("bi-heart-fill");
+    
+    await window.gsSDK.addInteraction({
+      "event": "like",
+      "item": productId
+    });
+}
 
 async function homeController(){
 
