d956e852746a2073e15904e799bd51f326363f0d
Santiago Cotto <scotto@goshops.ai>
2023-09-12 13:47:57 -0300

    sd

--- 3-home/app.js
+++ 3-home/app.js
@@ -77,6 +77,7 @@ $(document).ready(function() {
             })
             await window.gsSDK.addBulkInteractions(orders);
             window.location.href = '#'
+            window.location.reload();
         });
     });
 }
