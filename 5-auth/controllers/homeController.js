713f3b44745dab1558c96f279c1da7d30897a9a8
Santiago Cotto <scotto@goshops.ai>
2023-12-20 14:17:27 -0300

    aasd

--- 5-auth/controllers/homeController.js
+++ 5-auth/controllers/homeController.js
@@ -1,18 +1,18 @@
 
 async function handleHeartClick(event, productId) {
-    event.preventDefault();
-    
-    const heartIcon = event.currentTarget.querySelector("i.bi-heart");
-    heartIcon.classList.toggle("bi-heart-fill");
-    
-    await window.gsSDK.addInteraction({
-      "event": "like",
-      "item": productId
-    });
+  event.preventDefault();
+
+  const heartIcon = event.currentTarget.querySelector("i.bi-heart");
+  heartIcon.classList.toggle("bi-heart-fill");
+
+  await window.gsSDK.addInteraction({
+    "event": "like",
+    "item": productId
+  });
 }
 
-async function homeController(){
-
-    await window.gsSDK.getContentByContext('home');
+async function homeController() {
+  console.log('home controller');
+  await window.gsSDK.getContentByContext('home');
 }
 
